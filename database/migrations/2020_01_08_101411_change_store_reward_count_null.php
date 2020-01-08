49ecf2967bb14bc27fccd313095d230a99f25e08
Renish Backend <mail@globesynctechnologies.com>
2020-01-08 15:46:15 +0530

    Api change

---
+++ database/migrations/2020_01_08_101411_change_store_reward_count_null.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class ChangeStoreRewardCountNull extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('store_rewards', function (Blueprint $table) {
+            DB::statement("ALTER TABLE `store_rewards` CHANGE `count` `count` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('store_rewards', function (Blueprint $table) {
+            //
+        });
+    }
+}
