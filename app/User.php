28747e994a5d58ec278ec6425978597e312b7693
Renish Backend <mail@globesynctechnologies.com>
2020-01-20 19:13:18 +0530

    Api changes

--- app/User.php
+++ app/User.php
@@ -34,7 +34,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'tid',
         'is_active',
         'birthdate',
-        'gender'
+        'gender',
+        'is_notification'
     ];
 
     /**
